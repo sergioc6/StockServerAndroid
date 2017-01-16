87053d5cbc248778d909791585312056a6afc6f3
SergioC <sergiocabral.1990@gmail.com>
2017-01-16 19:39:59 -0300

    Andando el Consultar Stock por código QR. Dedicado a Nahuel y al amigo august

--- app/src/main/java/sergioc6/stockserverandroid/ConsultarStock.java
+++ app/src/main/java/sergioc6/stockserverandroid/ConsultarStock.java
@@ -2,27 +2,27 @@ package sergioc6.stockserverandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.vision.barcode.Barcode;
+import com.google.zxing.Result;
 
 import org.json.JSONException;
 
 import CustomerAPI.CustomerInsumos;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 /**
  * Created by SergioC on 12/01/2017.
  */
 
-public class ConsultarStock    extends AppCompatActivity {
+public class ConsultarStock extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-    private static final String LOG_TAG = ConsultarStock.class.getSimpleName();
-    private static final int BARCODE_READER_REQUEST_CODE = 1;
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +39,30 @@ public class ConsultarStock    extends AppCompatActivity {
     }
 
     public void consultarScanQR (View v) {
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view<br />
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.<br />
-        mScannerView.startCamera();
+       mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+       setContentView(mScannerView);
+       mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+       mScannerView.startCamera(0);
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();   // Stop camera on pause<br />
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        TokenApplication token = TokenApplication.getInstance();
+        try {
+            CustomerInsumos customerInsumos = new CustomerInsumos(token.getTokenGlobal(), getApplicationContext());
+            customerInsumos.consultarStockDeInsumo(rawResult.getText());
+        }
+        catch (JSONException jsExc)
+            {
+
+        }
+    }
 
 
     public void volverClick (View v) {
---
+++ app/src/main/java/sergioc6/stockserverandroid/barcodereader/BarcodeCaptureActivity.java
@@ -0,0 +1,8 @@
+package sergioc6.stockserverandroid.barcodereader;
+
+/**
+ * Created by SergioC on 16/01/2017.
+ */
+
+public class BarcodeCaptureActivity {
+}
